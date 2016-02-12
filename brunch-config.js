56b31324d00dd8229e611fb036504e290209fe2f
M4D <code@m4dz.net>
2016-02-12 10:01:59 +0100

    Fix Babel / Brunch / Underscore triumvirate

--- brunch-config.js
+++ brunch-config.js
@@ -3,13 +3,6 @@ exports.config = {
     enabled: true
   },
 
-  plugins: {
-    babel: {
-      presets: ['es2015'],
-      plugins: ["transform-es2015-modules-commonjs"]
-    }
-  },
-
   files: {
     javascripts: {
       joinTo: 'app.js'
@@ -18,7 +11,6 @@ exports.config = {
       joinTo: 'app.css'
     },
     templates: {
-      defaultExtension: 'jst',
       joinTo: 'app.js'
     }
   }
