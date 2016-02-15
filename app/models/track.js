df79d720849f01aa0fab225cd88d453c610c0b46
flyingrub <flyinggrub@gmail.com>
2016-02-15 14:07:12 +0100

    Test: cozy sdk

--- app/models/track.js
+++ app/models/track.js
@@ -1,15 +1,48 @@
 import Backbone from 'backbone'
-import CozySDK from '/vendor/CozySdk'
+
+cozysdk.defineRequest('files', 'getMusic', (doc) => {
+    type = file.headers['content-type']
+    if (type.split('/')[0] == 'audio') {
+        emit(doc);
+    }
+});
+
+cozysdk.run('Files', 'getMusic', {}, (error, response) => {
+    console.log(error, response);
+});
 
 const Track = Backbone.Model.extend({
-	urlRoot: '/track',
+    urlRoot: '/track',
     defaults: {
-	    track: 'FOO',
-	    artist: 'BAR'
-	},
-	sync: (method, model, options) => {
-
-	}
+        id: '',
+        track: '',
+        artist: ''
+    },
+    sync: (method, model, options) => {
+        switch (method) {
+            case 'create':
+                cozysdk.create('Track', model, (error, response) => {
+                    console.log(error, response);
+                });
+                break;
+            case 'read':
+                cozysdk.find('Track', model.id, (error, response) => {
+                    console.log(error, response);
+                 });
+                break;
+            case 'update':
+                cozysdk.updateAttributes(
+                    'Track', model.id, model, (error, response) => {
+                    console.log(error, response);
+                });
+                break;
+            case 'delete':
+                cozysdk.destroy('Track', model.id, (error, response) => {
+                    console.log(error, response);
+                });
+                break;
+        }
+    }
 });
 
 
