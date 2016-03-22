2d94a44e3edbfe810dce6e2644e642ac7d495666
flyingrub <flyinggrub@gmail.com>
2016-03-22 19:07:49 +0100

    [WIP] Refactor UI

--- app/views/track.js
+++ app/views/track.js
@@ -7,14 +7,14 @@ const TrackView = Mn.ItemView.extend({
 
     template: require('./templates/track'),
 
-    tagName: 'tr',
+    tagName: 'li',
 
     events: {
         'click': 'play',
         'click .delete': 'delete',
         'click #menu': 'displayMenu',
         'click #add-to-upnext':'addToUpNext',
-        'click "delete-from-upnext': 'deleteFromUpNext'
+        'click #delete-from-upnext': 'deleteFromUpNext'
     },
 
     modelEvents: {
@@ -46,6 +46,18 @@ const TrackView = Mn.ItemView.extend({
 
     play(e) {
         application.appState.set('currentTrack', this.model);
+    },
+
+    displayMenu(e) {
+        e.stopPropagation();
+    },
+
+    addToUpNext(e) {
+        e.stopPropagation();
+    },
+
+    deleteFromUpNext(e) {
+        e.stopPropagation();
     },
 
     delete(e) {
